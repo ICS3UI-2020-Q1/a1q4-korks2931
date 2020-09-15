e15efb94540aeb15dd91aab12c213c3adc459557
korks2931 <korks2931@wrdsb.ca>
2020-09-15 00:32:05 +0000

    finished a1q4

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input 
+    Scanner input = new Scanner (System.in);
+    // ask for the x coordinate of the first point 
+    System.out.println ("Please enter in the x coordinate of the first point");
+    //declare a variable to store x1
+    int x1 = input.nextInt();
+    //ask for the y coordinate of the first point 
+    System.out.println ("Please enter in the y coordinate of the first point");
+    //declare a variable to store y1
+    int y1 = input.nextInt();
+    //ask for the x coordinate of the second point 
+    System.out.println ( "Please enter in the x coordinate of the second point");
+    //declare a variable to store x2
+    int x2 = input.nextInt();
+    //ask for the y coordinate of the second point
+    System.out.println ("Please enter in the y coordinate of the second point");
+    //declare a variable to store y2
+    int y2 = input.nextInt();
+    // declare a variable to store slope 
+    double slope = (y2 - y1) / (x2 - x1);
+    // show the slope of the line
+    System.out.println ("The slope of your line would be " + slope +  ".");
+
     
   }
 }
